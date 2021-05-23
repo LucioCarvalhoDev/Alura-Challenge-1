515942dc0efae45c19a09c802d7ca8e5e3f2666c
lucio <luciocarvalhodev@gmail.com>
2021-05-23 10:43:19 -0300

    melhorias em noTabEscape e comentarios

--- alura-dev/js/codeEditor/noTabEscape.js
+++ alura-dev/js/codeEditor/noTabEscape.js
@@ -5,10 +5,24 @@ const recordKeyPressed = (event) => {
     lastKeyPressed = event.keyCode;
 }
 
+const tabStr = "  "
+
 const refocusEditor = () => {
+    //Ultima tecla pressionada foi TAB?
     if (lastKeyPressed != 9) return;
+
+    const caretPosition = [textArea.selectionStart, textArea.selectionEnd]
+
     textArea.focus();
-    textArea.value += "  ";
+
+    let newText = textArea.value.split('')
+    newText.splice(caretPosition[0], 0, tabStr);
+
+    textArea.value = newText.join("");
+
+    //Posiciona cursor corretamente
+    textArea.selectionStart = caretPosition[0] + tabStr.length;
+    textArea.selectionEnd = caretPosition[1] + tabStr.length;
 }
 
 textArea.addEventListener("keydown", recordKeyPressed);
