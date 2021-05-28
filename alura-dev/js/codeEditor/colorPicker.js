356dcd17f8ad280302a55157edb7c5e3b9e27587
lucio <luciocarvalhodev@gmail.com>
2021-05-28 18:25:25 -0300

    refatoração colorPicker

--- alura-dev/js/codeEditor/colorPicker.js
+++ alura-dev/js/codeEditor/colorPicker.js
@@ -1,23 +1,11 @@
 
 (() => {
-    const button = document.querySelector("[data-color-picker]");
-const codeEditor = document.querySelector(".m_code_editor");
+    const picker = document.getElementById("color-picker");
+    const bg = document.getElementById("bg-color-picker");
+    const codeEditor = document.querySelector(".m_code_editor");
 
-const colorClasses = ["red", "blue", "green", "white"];
-
-let i = -1;
-
-button.addEventListener("click", switchColors)
-
-function switchColors(event={preventDefault: ()=>{}}) {
-    event.preventDefault();
-
-    button.classList.remove(colorClasses[i]);
-    codeEditor.classList.remove(colorClasses[i]);
-    i == colorClasses.length -1 ? i = 0 : i++;
-    button.classList.add(colorClasses[i])
-    codeEditor.classList.add(colorClasses[i]);
-}
-
-switchColors();
-})()
+    picker.onchange = () => {
+        codeEditor.style.background = picker.value;
+        bg.style.background = picker.value;
+    };
+})();
