4a51e855e7dfb09d96cbb7ad966840927f86e4c0
lucio <luciocarvalhodev@gmail.com>
2021-05-30 15:07:20 -0300

    persistencia para pagina home implementado

--- alura-dev/js/dao/save.js
+++ alura-dev/js/dao/save.js
@@ -1,23 +1,89 @@
-const trigger = document.getElementById("save");
+//configurações banco de dados
+const database = {
+    DB: undefined,
+    init: function () {
+        return new Promise((resolve, reject) => {
+            const request = window.indexedDB.open('alura-dev', 1);
 
-//inputs
-const editor = document.getElementById("editor-text");
-const langSelector = document.getElementById("lang-selector");
-const name = document.getElementById("ipt-name");
-const description = document.getElementById("ipt-description");
-const color = document.getElementById("color-picker");
+            request.onsuccess = (event) => {
+                this.DB = request.result;
+                resolve();
+            };
+
+            request.onerror = (event) => {
+                reject();
+            };
+
+            request.onupgradeneeded = (event) => {
+                this.DB = event.target.result;
+
+                if (this.DB.objectStoreNames.contains("codes")) return;
+
+                this.DB.createObjectStore("codes", { autoIncrement: true });
+            };
+        });
 
 
-trigger.onclick = function (event) {
-    event.preventDefault();
 
-    const data = {
-        text: editor.value,
-        lang: langSelector.value,
-        name: name.value,
-        description: description.value,
-        color: color.value
-    };
+    },
+    add: function (data) {
+        return new Promise((resolve, reject) => {
 
-    console.log(data);
+            let transaction = this.DB.transaction(["codes"], "readwrite");
+            transaction.onerror = () => reject([data, "error"]);
+
+            const objectStore = transaction.objectStore("codes");
+            const request = objectStore.add(data);
+
+            request.onsuccess = () => resolve([data, "ok"]);
+        });
+    },
+    load: function () {
+        return new Promise((resolve, reject) => {
+            const dataset = [];
+
+            const transaction = this.DB.transaction(["codes"]);
+
+            const objectStore = transaction.objectStore("codes");
+
+            objectStore.openCursor().onsuccess = function (event) {
+                const cursor = event.target.result;
+
+                if (cursor) {
+                    dataset.push(cursor.value);
+                    cursor.continue();
+                } else {
+                    resolve(dataset);
+                }
+            };
+        });
+    }
 };
+
+database.init()
+    .then(() => {
+
+        const trigger = document.getElementById("save");
+
+        //inputs
+        const editor = document.getElementById("editor-text");
+        const langSelector = document.getElementById("lang-selector");
+        const name = document.getElementById("ipt-name");
+        const description = document.getElementById("ipt-description");
+        const color = document.getElementById("color-picker");
+
+
+        trigger.onclick = function (event) {
+            event.preventDefault();
+
+            const data = {
+                text: editor.value,
+                lang: langSelector.value,
+                name: name.value,
+                description: description.value,
+                color: color.value
+            };
+
+            database.add(data);
+        };
+    });
